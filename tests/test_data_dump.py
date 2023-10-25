7b4d74cbd8476cf1620133b48050dbffa4f97914
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-10-25 17:50:55 +0200

    Schedule a full data dump test every month

--- tests/test_data_dump.py
+++ tests/test_data_dump.py
@@ -7,7 +7,7 @@ from py3data import Repositories
 def test_data_dump(tmpdir):
     export_f = Path(tmpdir, "datadump.json")
 
-    all_data = [Repositories()[x["id"]] for x in Repositories().get()[0:5]]
+    all_data = [Repositories()[x["id"]] for x in Repositories().get()]
 
     with open(export_f, "w") as f:
         json.dump(all_data, f)
