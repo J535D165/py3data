eae1b3c67039226b3c85f91eac041745ca7f9b1a
Jonathan de Bruin <jonathandebruinos@gmail.com>
2023-10-25 17:46:17 +0200

    Fix data dump test

--- tests/test_data_dump.py
+++ tests/test_data_dump.py
@@ -10,4 +10,4 @@ def test_data_dump(tmpdir):
     all_data = [Repositories()[x["id"]] for x in Repositories().get()[0:5]]
 
     with open(export_f, "w") as f:
-        json.dumps(all_data, f)
+        json.dump(all_data, f)
